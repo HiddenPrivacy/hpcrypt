9cb144476f551c3d64735e0ffbbe8455f286c922
wikin <wiki@atlas.cz>
2020-08-25 16:49:19 +0200

    fix param name in docs

--- encryptText.js
+++ encryptText.js
@@ -4,12 +4,12 @@ import textEncode from './utils/textEncode'
 /**
  * Encrypt string with AES and RSA algorithms and return base64 encoded cipher
  * @async
- * @param {string} privateKey - RSA private key in PEM format
+ * @param {string} publicKey - RSA private key in PEM format
  * @param {string} text - string to encrypt
  * @returns {string} - base64 encoded cipher
  * @throws {Error}
  */
-export default async function(privateKey, text) {
+export default async function(publicKey, text) {
   let buffer = textEncode(text)
-  return encrypt(privateKey, buffer)
+  return encrypt(publicKey, buffer)
 }
